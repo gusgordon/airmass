9126cf885adfdccb71bc3c68dbcab572226df61a
Gus Gordon <ggordon@quantopian.com>
2020-02-07 17:56:11 -0500

    Add imports

--- airmass/__init__.py
+++ airmass/__init__.py
@@ -1,3 +1,8 @@
 from airmassc import c_compute_airmass as _compute_airmass
-from .airmass import compute, from_altitude
-from .solar import solar_intensity, yearly_solar_intensity
+from .airmass import compute, from_altitude, water_vapor_col_density
+from .solar import (
+    get_solar_spectrum_modtran, solar_intensity, yearly_solar_intensity,
+    get_ozone_ppm, get_cross_section, get_air_mass_extinction,
+    get_ozone_particle_col_density, get_cloud_attentuation,
+    get_solar_irradiance,
+)
