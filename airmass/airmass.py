1d4263c7ad9f4c214a9cf5cc5826caaa068e46f8
Gus Gordon <ggordon@quantopian.com>
2020-02-07 17:42:58 -0500

    Compute solar irradiance using extinction

--- airmass/airmass.py
+++ airmass/airmass.py
@@ -1,27 +1,10 @@
-import os
 import numpy as np
 import pandas as pd
 from airmassc import c_compute_airmass as _compute_airmass
 
-# Atmospheric data from http://www.digitaldutch.com/atmoscalc/tableatmosphere.htm
-dir_path = os.path.dirname(os.path.realpath(__file__))
-atm_data = pd.read_csv(os.path.join(dir_path, "st_atm_1976.csv")).set_index("Altitude [m]")
-atmosphere = (
-    atm_data.reindex(np.arange(atm_data.index[0], atm_data.index[-1] + 1))
-    .interpolate()
-    .values
-)
-
 
 def compute(
-    zenith_degrees,
-    altitude,
-    day_of_year,
-    latitude,
-    light_wavelength,
-    P_Pa,
-    T_K,
-    relative_humidity=None,
+    zenith_degrees, altitude, day_of_year, latitude, light_wavelength, P_Pa, T_K
 ):
     """Computes airmass and column density on Earth for a
     given wavelength of light.
@@ -60,43 +43,21 @@ def compute(
         Consider using the ``from_altitude`` function below if you are using
         standard atmospheric conditions, and would like temperature and pressure
         automatically applied for your supplied altitude.
-    relative_humidity : float, default None
-        Additionally compute an estimate of the water vapor column density
-        and airmass, using a crude model for the water vapor profile based
-        on the local relative humidity at ground level in fraction (0.0 = dry, 1.0 = saturated).
 
     Returns
     -------
     dict
-        Contains airmass and column densities, for dry air by default and for water
-        vapor if relative_humidity is specified.
+        Contains airmass and column densities for dry air.
     """
-    if relative_humidity is None:
-        res = _compute_airmass(
-            zenith_degrees, altitude, day_of_year, latitude, light_wavelength, P_Pa, T_K
-        )
-    else:
-        res = _compute_airmass(
-            zenith_degrees,
-            altitude,
-            day_of_year,
-            latitude,
-            light_wavelength,
-            P_Pa,
-            T_K,
-            relhumid=relative_humidity,
-        )
+
+    res = _compute_airmass(
+        zenith_degrees, altitude, day_of_year, latitude, light_wavelength, P_Pa, T_K
+    )
 
     if res[0] == "zenith_degrees not valid":
         raise ValueError(
             f"Zenith angle of {zenith_degrees} degrees is not valid. The zenith angle must be between 0 and 90 degrees."
         )
-    elif res[0] == "water vapor":
-        return {
-            "type": "water_vapor_airmass",
-            "water_vapor_column_density": res[1],
-            "water_vapor_airmass": res[2],
-        }
     elif res[0] == "regular":
         return {
             "type": "standard_airmass",
@@ -108,9 +69,99 @@ def compute(
         raise Exception("Internal exception occurred. Try another set of values.")
 
 
-def from_altitude(
-    zenith_degrees, altitude, day_of_year, latitude, light_wavelength, relative_humidity=None
+def water_vapor_col_density(
+    zenith_degrees,
+    altitude,
+    day_of_year,
+    latitude,
+    light_wavelength,
+    relative_humidity_sea_level,
 ):
+    """Computes a rough approximation for the column density of water vapor.
+
+    Reed's program has two issues with the humidity calculations:
+      1. It uses a relative humidity at altitude, whereas most relative humidity measurements
+      are at sea level. This version allow a relative humidity at sea level
+      to be specified.
+      2. Hangs on this calculation for altitudes greater than roughly
+      4000 m, so this functions attempts to "patch" that by computing the column
+      density at sea level then extrapolating to higher altitudes.
+
+    Since the original water vapor model is simple, this is relatively simple
+    to approximate roughly.
+
+    Parameters
+    ----------
+    zenith_degrees : float
+        The apparent zenith angle (not the true angle in the
+        absence of atmosphere, but angle including refraction) at which
+        you want the airmass.
+    altitude : float
+        The altitude of the observer above sea level, in meters.
+        Consider using the ``from_altitude`` function below if you are using
+        standard atmospheric conditions, and would like temperature and pressure
+        automatically applied for your supplied altitude.
+    day_of_year : float
+        The date, in days since the beginning of the year
+        (0 = midnight Jan. 1; the default is 80, corresponding roughly to
+        the vernal equinox); don't worry about fractions of a day, since the
+        seasonal effect on airmass is relatively small.
+    latitude : float
+        The observer's latitude on Earth.
+    light_wavelength : float
+        The observing wavelength in Angstroms. Don't worry too much about an
+        exact figure here, as the wavelength dependence is very small.
+    P_Pa : float
+        Specifies the local pressure in Pascals.
+        Consider using the ``from_altitude`` function below if you are using
+        standard atmospheric conditions, and would like temperature and pressure
+        automatically applied for your supplied altitude.
+    T_K : float
+        Specifies the local temperature in Kelvin.
+        Consider using the ``from_altitude`` function below if you are using
+        standard atmospheric conditions, and would like temperature and pressure
+        automatically applied for your supplied altitude.
+
+    Returns
+    -------
+    float
+        Water vapor column density.
+    """
+
+    troposphere_top_m = 17000
+    lower_stratosphere_top_m = 30000
+
+    if altitude < troposphere_top_m:
+        rel_humid = sea_level_humidity
+    elif altitude < lower_stratosphere_top_m:
+        rel_humid = sea_level_humidity * 0.5
+    else:
+        rel_humid = 0
+
+    water_vapor_col_density_0_alt = (
+        _compute_airmass(
+            zenith_degrees,
+            0,
+            day_of_year,
+            latitude,
+            light_wavelength,
+            101325,
+            288.15,
+            relative_humidity_sea_level,
+        )[1]
+        * -0.5
+    )
+
+    mean_humid_alt_falloff = (troposphere_top_m + lower_stratosphere_top_m) / 2
+
+    water_vapor_col_density = max(
+        (1 - altitude / mean_humid_alt_falloff) * water_vapor_col_density_0_alt, 0
+    )
+
+    return water_vapor_col_density
+
+
+def from_altitude(zenith_degrees, altitude, day_of_year, latitude, light_wavelength):
     """Computes airmass and column density on Earth for a
     given altitude. Uses the 1976 atmospheric data to compute temperature and
     pressure for the given altitude. If you would like to specify temperature and
@@ -134,17 +185,21 @@ def from_altitude(
     light_wavelength : float
         The observing wavelength in Angstroms. Don't worry too much about an
         exact figure here, as the wavelength dependence is very small.
-    relative_humidity : float, default None
-        Additionally compute an estimate of the water vapor column density
-        and airmass, using a crude model for the water vapor profile based
-        on the local relative humidity at ground level in fraction (0.0 = dry, 1.0 = saturated).
 
     Returns
     -------
     dict
-        Contains airmass and column densities, for dry air by default and for water
-        vapor if relative_humidity is specified.
+        Contains airmass and column densities for dry air.
     """
+
+    # Atmospheric data from http://www.digitaldutch.com/atmoscalc/tableatmosphere.htm
+    atm_data = pd.read_csv("data/st_atm_1976.csv").set_index("Altitude [m]")
+    atmosphere = (
+        atm_data.reindex(np.arange(atm_data.index[0], atm_data.index[-1] + 1))
+        .interpolate()
+        .values
+    )
+
     T_K, P_Pa, _, _, _ = atmosphere[int(altitude)]
 
     return compute(
--- airmass/solar.py
+++ airmass/solar.py
@@ -1,101 +1,371 @@
 import numpy as np
 import pandas as pd
-from . import airmass as airmass_lib
+import airmass as airmass_lib
+
+N_Avogadro = 6.02214076e23
+
+atmosphere_composition_by_mass = {
+    "N2": 0.75519454,
+    "O2": 0.23140128,
+    "Ar": 0.01288169,
+    "CO2": 0.0004771,
+    "SO2": 0.00000221,
+    "CH4": 0.00000111,
+}
+
+molar_mass_g_per_mol = {
+    "N2": 28.0134,
+    "O2": 31.9988,
+    "Ar": 39.948,
+    "CO2": 44.0095,
+    "SO2": 64.0638,
+    "CH4": 16.04246,
+}
+
+air_molar_mass = 0.0289647  # kg/mol
 
 
-def solar_intensity(
-    altitude,
-    day_of_year,
-    latitude,
-    wavelength=5000,
-    hour_of_day=None,
-    include_albedo=False,
-):
+def get_solar_spectrum_modtran():
     """
-    Experimental - outputs solar intensities for a given wavelength of light.
+    Load the solar spectrum from MODTRAN 6.
 
-    Todo:
-     - Don't use airmass, but use column density with the extinction coefficients of atmospheric
-       components (dry air, ozone, dust, and water vapor). Then compute intensity using
-       Reed's exctinction formula.
+    Returns
+    -------
+    pd.DataFrame
+        Solar spectrum in increments of 0.1 nm, containing
+        irradiance and power contribution for the wavelength.
     """
-    albedo_zero_zenith = 0.25
-    solar_irradiance_space = 1367.0  # W/m^2
-    earth_radius = 6375000  # m
+    # Load in MODTRAN extraterrestrial specta
+    # Source: https://www.nrel.gov/grid/solar-resource/spectra.html
+    solar_spectrum_all = (
+        pd.read_csv("data/MODTRAN-W_M-2_nm-1.csv")
+        .drop("(CM-1)", axis=1)
+        .set_index("nm")
+    )
 
-    # Declination angle from http://www.pveducation.org/pvcdrom/properties-of-sunlight/declination-angle
-    declination = -23.45 * np.cos(360 / 365 * (day_of_year + 10) * np.pi / 180)
+    # For each wavelength, take the mean across all samples
+    solar_spectrum_mean = solar_spectrum_all.mean(axis=1)
 
-    # Loop through day's hours
-    if hour_of_day is None:
-        hour_angles = np.arange(0, 360)
+    # Reduce the precision to something more reasonable
+    reduced_solar_spectrum = solar_spectrum_mean.groupby(
+        solar_spectrum_mean.index.to_series().round(1)
+    ).mean()
+
+    # Also compute the power contribution for each wavelength
+    # This gives a high estimate, as there is imprecision on the high end of the spectrum
+    # For cases involving solar cells, the high end of the spectrum is cut out anyway,
+    # so it doesn't matter here.
+    reduced_solar_spectrum = pd.DataFrame(
+        {"irradiance": reduced_solar_spectrum}
+    ).reset_index()
+    reduced_solar_spectrum["wavelength_width"] = reduced_solar_spectrum.nm.diff()
+    reduced_solar_spectrum.loc[0, "wavelength_width"] = 0.1
+    reduced_solar_spectrum["power_contribution"] = (
+        reduced_solar_spectrum.irradiance * reduced_solar_spectrum.wavelength_width
+    )
+
+    return reduced_solar_spectrum.set_index("nm")
+
+
+def get_ozone_ppm():
+    """
+    Load the ozone concentration at different altitudes.
+
+    Returns
+    -------
+    pd.DataFrame
+        Solar spectrum in increments of 0.1 nm, containing
+        irradiance and power contribution for the wavelength.
+    """
+
+    # Load in NASA ozone vs. altitude data
+    # Source: https://ozonewatch.gsfc.nasa.gov/facts/SH.html
+    ozone_ppm_vs_altitude = pd.read_csv(
+        "data/ozone_vs_altitude.csv", index_col="altitude_km"
+    ).ozone_ppm.sort_index()
+
+    ozone_frac_vs_altitude = ozone_ppm_vs_altitude * 1e-6
+    ozone_frac_vs_altitude.index = ozone_frac_vs_altitude.index * 1e3
+    ozone_frac_vs_altitude.index.name = "altitude_m"
+
+    new_index = np.concatenate(
+        [
+            ozone_frac_vs_altitude.index,
+            np.arange(0, ozone_frac_vs_altitude.index[-1] + 1),
+        ]
+    )
+
+    # Interpolate ozone for all altitudes
+    ozone_frac_vs_altitude = (
+        (ozone_frac_vs_altitude.reindex(new_index))
+        .sort_index()
+        .interpolate(method="cubic")
+    )
+
+    # Fill low altitude values as 0 concentration
+    ozone_frac_vs_altitude = ozone_frac_vs_altitude.fillna(0)
+
+    # Round the altitude to the nearest meter so it's easier to query
+    ozone_frac_vs_altitude.index = ozone_frac_vs_altitude.index.to_series().round()
+
+    return ozone_frac_vs_altitude
+
+
+def get_cross_section(compound):
+    """
+    Load the extinction cross section (absorbtion + Rayleigh) for a
+    given compound. Other sources of extinction are negligible.
+    Returns cross sections in units of cm^2.
+
+    This data was generated using the generate_extinction.ipynb
+    notebook in the etc/ directory. The basis of this notebook
+    is https://github.com/sukritranjan/ranjanwordsworthsasselov2016/blob/bd5de2cb081b129bfd6b3b5bc173ca4e9ce7702b/extract_gas_cross_sections.py.
+
+    Supported compounds:
+     - N2
+     - Ar (absorbtion cross section is ignored, need to add this, but it's negligible)
+     - O2
+     - CO2
+     - H2O
+     - CH4
+     - O3
+     - SO2
+
+    Returns
+    -------
+    pd.DataFrame
+        Total extintion cross section in increments of 0.1 nm.
+    """
+
+    cross_section = pd.read_csv(
+        f"data/composite_xc_extended_{compound.lower()}",
+        delim_whitespace=True,
+        index_col="wav_nm",
+    )
+
+    return cross_section
+
+
+def get_air_mass_extinction():
+    """
+    Load the total extinction cross section (absorbtion + Rayleigh)
+    for air in cm^2/g.
+
+    Included compounds:
+     - N2
+     - Ar (absorbtion cross section is ignored, need to add this, but it's negligible)
+     - O2
+     - CO2
+     - CH4
+     - SO2
+
+    Returns
+    -------
+    pd.Series
+        Total extintion cross section of air in increments of 0.1 nm.
+    """
+
+    xs = {}
+    for molecule, contribution in atmosphere_composition_by_mass.items():
+        xs[molecule] = get_cross_section(molecule).total_cm2.mul(
+            N_Avogadro / molar_mass_g_per_mol[molecule]
+        )
+
+        xs[molecule] = xs[molecule].mul(contribution)
+
+        xs[molecule] = (
+            xs[molecule].groupby(xs[molecule].index.to_series().round(1)).mean()
+        )
+
+    # Interpolate missing values linearly. This mostly fills in the CO2 spectrum
+    xs = pd.DataFrame(xs)[200:900].interpolate()
+
+    return xs.sum(axis=1)
+
+
+def get_ozone_particle_col_density(zenith_degrees, altitude):
+    """Computes the ozone particle column density in particles/m^2.
+
+    Parameters
+    ----------
+    zenith_degrees : float
+        The apparent zenith angle (not the true angle in the
+        absence of atmosphere, but angle including refraction) at which
+        you want the airmass.
+    altitude : float
+        The altitude of the observer above sea level, in meters.
+
+    Returns
+    -------
+    float
+        Ozone particle column density in particles/m^2.
+    """
+
+    atm_data = pd.read_csv("data/st_atm_1976.csv").set_index("Altitude [m]")
+    atmosphere = atm_data.reindex(
+        np.arange(atm_data.index[0], atm_data.index[-1] + 1)
+    ).interpolate()
+
+    n_air_particles = atmosphere["Density [kg/m3]"] / air_molar_mass * N_Avogadro
+
+    ozone = pd.DataFrame({"ppm": get_ozone(), "n_air_particles": n_air_particles})
+    ozone["rel_ppm"] = ozone.ppm.div(np.cos(zenith_degrees * np.pi / 180))
+    ozone = ozone[:50000]
+    ozone["ozone_particles"] = ozone.n_air_particles * ozone.rel_ppm / 1e6
+
+    return ozone[altitude:].ozone_particles.sum()
+
+
+def get_cloud_attentuation(altitude, cloud_level_m=9000):
+    """Computes the cloud attenuation using a simple model of a single
+    cloud at the given altitude.
+    
+    Note this is effectively the worst case scenario for cloud cover.
+
+    Parameters
+    ----------
+    altitude : float
+        The altitude of the observer above sea level, in meters.
+
+    Returns
+    -------
+    float
+        Cloud attenuation.
+    """
+
+    if altitude > cloud_level_m:
+        return 0
     else:
-        hour_angles = [int(np.rint((hour_of_day * 360) / 24)) + 180]
-
-    intensities = []
-    for hour_angle in hour_angles:
-        p1 = np.sin(latitude * np.pi / 180) * np.sin(declination * np.pi / 180)
-        p2 = (
-            np.cos(latitude * np.pi / 180)
-            * np.cos(declination * np.pi / 180)
-            * np.cos(hour_angle * np.pi / 180)
-        )
-        zenith = np.arccos(p1 + p2) * 180 / np.pi
-
-        if zenith > 89.9:
-            intensities.append(0)
-            continue
-
-        airmass = airmass_lib.from_altitude(
-            zenith, altitude, day_of_year, latitude, wavelength
-        )['airmass']
-        airmass_sea_level_zenith = airmass_lib.from_altitude(
-            0, 0, day_of_year, latitude, wavelength
-        )['airmass']
-        relative_airmass = airmass / airmass_sea_level_zenith
-
-        # Modified from http://www.pveducation.org/pvcdrom/2-properties-sunlight/air-mass
-        intensity = (
-            (1 + min(relative_airmass, 1) / 10)
-            * 1353
-            * 0.7 ** (relative_airmass ** 0.678)
-            * np.cos(zenith * np.pi / 180)
-        )
-
-        # Albedo model: http://slideplayer.com/slide/10671870/
-        if include_albedo:
-            albedo = (
-                albedo_zero_zenith
-                + 4.9115e-9 * zenith ** 4
-                + 6.0372e-8 * zenith ** 3
-                - 2.1793e-5 * zenith ** 2
-                + 1.3798e-3 * zenith
-            )
-            local_ff = (
-                earth_radius / (earth_radius + altitude)
-            ) ** 2  # Local form factor to Earth
-            albedo_intensity = (
-                solar_irradiance_space
-                * albedo
-                * local_ff
-                * np.cos(zenith * np.pi / 180)
-            )
-            intensity += albedo_intensity
-
-        intensities.append(intensity)
-
-    return np.mean(np.concatenate([intensities]))
+        return 0.85
 
 
-def yearly_solar_intensity(altitude, latitude=20, wavelength=5000, cloud_cover=None):
-    mis = []
-    for day in np.arange(0, 359, 30):
-        mi = solar_intensity(
-            altitude, day_of_year=day, latitude=latitude, wavelength=wavelength
-        )
-        mis.append(mi)
-    mis_mean = np.mean(mis)
-    if cloud_cover:
-        attn = 0.135  # Clouds only let 13.5% of light through on average
-        mis_mean = cloud_cover * mis_mean * attn + (1 - cloud_cover) * mis_mean * 1
-    return mis_mean, mis_mean * 60 * 60 * 24 / (1000 * 3600)
+def get_solar_irradiance(
+    zenith_degrees, altitude, day_of_year, latitude, include_diffuse_sky=True
+):
+    """Get the solar irradiance in watts/m^2 at a certain altitude and given certain conditions.=
+    Utilizes exctinction coefficients across 200-900 nm wavelengths for dry air, water vapor, ozone,
+    and clouds (using a simple model for clouds).
+    
+    Wavelengths outside of 200-900 nm are extrapolated, however this is roughly valid given that
+    Rayleigh scattering is the primary contributor to extinction and it is fairly consistent at
+    the larger wavelengths.
+    
+    A way to improve this function would be to incorporate extinction coefficients at larger
+    wavelengths.
+    
+    Outputs power in watts/m^2.
+
+    Parameters
+    ----------
+    zenith_degrees : float
+        The apparent zenith angle (not the true angle in the
+        absence of atmosphere, but angle including refraction) at which
+        you want the airmass.
+    altitude : float
+        The altitude of the observer above sea level, in meters.
+        Consider using the ``from_altitude`` function below if you are using
+        standard atmospheric conditions, and would like temperature and pressure
+        automatically applied for your supplied altitude.
+    day_of_year : float
+        The date, in days since the beginning of the year
+        (0 = midnight Jan. 1; the default is 80, corresponding roughly to
+        the vernal equinox); don't worry about fractions of a day, since the
+        seasonal effect on airmass is relatively small.
+    latitude : float
+        The observer's latitude on Earth.
+    include_diffuse_sky : boolean, default True
+        Approximately add in the contribution from diffuse sky irradiance.
+        This is a rough model, but regardless, the contribution is fairly small.
+
+    Returns
+    -------
+    float
+        Solar irradiance in watts/m^2.
+    """
+
+    # Dry air
+    cd = {}
+    for wavelength in np.arange(200, 900.1, 0.1):
+        if np.round(wavelength, 1) % 10 == 0:
+            cd[wavelength] = airmass_lib.from_altitude(
+                zenith_degrees, altitude, day_of_year, latitude, wavelength * 10
+            )["column_density"]
+        else:
+            cd[wavelength] = np.nan
+
+    wavelength_col_densities = pd.Series(cd).interpolate()
+
+    wavelength_col_densities.index = wavelength_col_densities.index.to_series().round(1)
+
+    air_extinction_coefs = wavelength_col_densities.mul(get_air_mass_extinction())
+
+    # Ozone
+    o3_cross_section = get_cross_section("O3").total_cm2
+    o3_cross_section = o3_cross_section.reindex(
+        np.concatenate([o3_cross_section.index, np.arange(200.0, 900.1, 0.1)])
+    ).sort_index()
+    o3_cross_section = o3_cross_section.interpolate()
+    o3_cross_section = o3_cross_section.groupby(
+        o3_cross_section.index.to_series().round(1)
+    ).mean()
+
+    ozone_extinction_coefs = o3_cross_section[200:900] * get_ozone_particle_col_density(
+        zenith_degrees, altitude
+    )
+
+    # Water vapor
+    wv_col_density = airmass_lib.water_vapor_col_density(
+        zenith_degrees, altitude, day_of_year, latitude, 5500, 0.5
+    )
+
+    h2o_cross_section = get_cross_section("h2o").total_cm2[200:900]
+    h2o_cross_section = h2o_cross_section.reindex(
+        np.concatenate([h2o_cross_section.index, np.arange(200.0, 900.1, 0.1)])
+    ).sort_index()
+    h2o_cross_section = h2o_cross_section.interpolate()
+    h2o_cross_section = h2o_cross_section.groupby(
+        h2o_cross_section.index.to_series().round(1)
+    ).mean()
+    h2o_extinction_coefs = h2o_cross_section.mul(N_Avogadro / 18.01528)
+
+    # Clouds
+    cloud_extinction = get_cloud_attentuation(altitude)
+
+    # Sum the extinctions to get the total extinction
+    total_extinction = (
+        air_extinction_coefs
+        + ozone_extinction_coefs
+        + h2o_extinction_coefs
+        + cloud_extinction
+    )
+
+    solar_spectrum_power = get_solar_spectrum_modtran().power_contribution
+
+    # Get the total power *in the 200-900 nm range*
+    visible_power_spectrum = solar_spectrum_power * np.power(np.e, -total_extinction)
+    visible_power_total = visible_power_spectrum.sum()
+
+    # Since we only had exctinction coefficients in the visible spectrum, let's multiply this back
+    # out as an approximation for the total spectrum. This should be roughly accurate, since most
+    # radiation is from Rayleigh scattering which is roughly constant for larger wavelengths.
+    frac_visible_power = (
+        solar_spectrum_power[200:900].sum() / solar_spectrum_power.sum()
+    )
+    total_power_approx = visible_power_total / frac_visible_power
+
+    # We use a very simple model for the diffuse sky irradiance
+    # since the diffuse sky irradiance is fairly insignificant.
+    # We assume that at sea level, there is an 8% contribution from diffuse
+    # sky, and this drops off linearly up until 20,000 m, above which the
+    # contribution is assumed to be zero.
+    diffuse_sky_factor = max(0.08 * min(1, (20000 - altitude) / 20000), 0)
+
+    # We also multiply by the cosine of the zenith angle to account for the projection
+    # of sunlight onto a flat surface
+    total_power_incedent = (
+        total_power_approx
+        * (1 + diffuse_sky_factor)
+        * np.cos(zenith_degrees * np.pi / 180)
+    )
+
+    return total_power_incedent
